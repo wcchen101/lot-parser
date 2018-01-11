752a9037aa054e013e81452cee54f49c81f884e7
Wei-Chung Chen <wcchen101@gmail.com>
2018-01-10 21:56:08 -0800

    add: parseHTML_1.py

---
+++ data/parseHTML_1.py
@@ -0,0 +1,20 @@
+import os
+from bs4 import BeautifulSoup
+
+targetFolder = '2015-03-18'
+htmlFiles, titleAll = [], []
+for subFile in os.listdir(targetFolder):
+	htmlFiles.append(subFile)
+
+for subFile in htmlFiles:
+	f = open(targetFolder+ '/' + subFile, 'r')
+	soup = BeautifulSoup(f, 'lxml')
+	for titleElement in soup.find_all('title'):
+		rawTitle = titleElement.text
+		refinedTitle = rawTitle.replace('\n', ' ')
+		refinedTitle = refinedTitle.strip()
+		breakIndex = refinedTitle.index(':')
+		titleAll.append(refinedTitle[:breakIndex])
+	f.close()
+
+print(titleAll)
