bb1a791ff198acf226ef393d19b872941fa4f406
Wei-Chung Chen <wcchen101@gmail.com>
2018-01-10 21:56:37 -0800

    add: parseHTML_3.py

---
+++ data/parseHTML_3.py
@@ -0,0 +1,52 @@
+import os
+from bs4 import BeautifulSoup
+from lxml import html
+
+targetFolder = '2015-03-18'
+htmlFiles, artistAll, targetObj = [], [], {}
+for subFile in os.listdir(targetFolder):
+	htmlFiles.append(subFile)
+
+for subFile in htmlFiles:
+	f = open(targetFolder+ '/' + subFile, 'r')
+	soup = BeautifulSoup(f, 'lxml')
+	divElementArr = []
+	worksObj = {
+		'title': '',
+		'price': 0
+	}
+	targetObj = {
+		'artist': '',
+		'works': worksObj
+	}
+	for titleElement in soup.find_all('title'):
+		rawTitle = titleElement.text
+		refinedTitle = rawTitle.replace('\n', ' ')
+		refinedTitle = refinedTitle.strip()
+		breakIndex = refinedTitle.index(':')
+		title = refinedTitle[:breakIndex]
+		work = refinedTitle[breakIndex + 1:].strip()
+		worksObj = {
+			'title': work,
+			'price': 0
+		}
+		targetObj = {
+			'artist': title,
+			'works': worksObj
+		}
+		artistAll.append(targetObj)
+
+	price = None
+	refinedDivArr = []
+	for divElement in soup.find_all('div'):
+		rawDiv = divElement.text
+		refinedDiv = rawDiv.replace('\n', ' ')
+		refinedDivArr.append(refinedDiv.strip())
+
+	price = refinedDivArr[1]
+	worksObj['price'] = price.strip()
+	targetObj['works'] = worksObj
+
+	artistAll.append(targetObj)
+	f.close()
+print(artistAll)
