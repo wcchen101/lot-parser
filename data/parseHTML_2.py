b754f0326be9c2ecf021f36cfec9e294275c6239
Wei-Chung Chen <wcchen101@gmail.com>
2018-01-10 21:56:24 -0800

    add: parseHTML_2.py

---
+++ data/parseHTML_2.py
@@ -0,0 +1,26 @@
+import os
+from bs4 import BeautifulSoup
+
+targetFolder = '2015-03-18'
+htmlFiles, artistAll, targetObj = [], [], {}
+for subFile in os.listdir(targetFolder):
+	htmlFiles.append(subFile)
+
+for subFile in htmlFiles:
+	f = open(targetFolder+ '/' + subFile, 'r')
+	soup = BeautifulSoup(f, 'lxml')
+	for titleElement in soup.find_all('title'):
+		rawTitle = titleElement.text
+		refinedTitle = rawTitle.replace('\n', ' ')
+		refinedTitle = refinedTitle.strip()
+		breakIndex = refinedTitle.index(':')
+		title = refinedTitle[:breakIndex]
+		work = refinedTitle[breakIndex + 1:].strip()
+		targetObj = {
+			'artist': title,
+			'work': work
+		}
+		artistAll.append(targetObj)
+	f.close()
+
+print(artistAll)
